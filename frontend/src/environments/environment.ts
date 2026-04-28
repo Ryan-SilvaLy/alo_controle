f0063254f2a7e885e85554b24ed3b16c48ebef4f
Your Name <you@example.com>
2026-04-27 23:13:38 -0300

    Criando api local

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,3 +2,10 @@ export const environment = {
   production: true,
   apiUrl: 'https://alocontrole-production.up.railway.app'
 };
+
+
+//   export const environment = {
+//   production: true,
+//   apiUrl: 'http://localhost:8000'
+// };
+
