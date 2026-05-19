64f75644b9cd358f14c0fde2e0dee57fef75a9c9
Ryan Silva <ryanlyestudos@gmail.com>
2026-05-19 16:58:36 -0300

    Corrigindo erro de imagem do codido de barras

--- backend/app_item/apis/serializers.py
+++ backend/app_item/apis/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from app_item.models import Item, TipoItem
+import base64
 import logging 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +28,7 @@ class ItemSerializer(serializers.ModelSerializer):
 
     # O objeto completo para leitura.
     tipo_item = TipoItemSerializer(read_only=True)
+    codigo_barras_imagem_base64 = serializers.SerializerMethodField()
 
     # No post para envio ao backend, é enviado apenas o id do TipoItem.
     tipo_item_id = serializers.PrimaryKeyRelatedField(
@@ -43,5 +45,22 @@ class ItemSerializer(serializers.ModelSerializer):
         read_only_fields = ['id', 'situacao', 'criado_em', 'atualizado_em']
 
     
+    def get_codigo_barras_imagem_base64(self, obj):
+        if not obj.codigo_barras:
+            return ''
+
+        if not obj.codigo_barras_imagem:
+            obj._gerar_imagem_codigo_barras()
+            obj.save(update_fields=['codigo_barras_imagem'])
+
+        try:
+            with obj.codigo_barras_imagem.open('rb') as arquivo:
+                return base64.b64encode(arquivo.read()).decode('ascii')
+        except FileNotFoundError:
+            obj._gerar_imagem_codigo_barras()
+            obj.save(update_fields=['codigo_barras_imagem'])
+            with obj.codigo_barras_imagem.open('rb') as arquivo:
+                return base64.b64encode(arquivo.read()).decode('ascii')
+
     # def get_unidade_medida(self, obj):
     #     return obj.get_unidade_medida_display()
